12eb130bf5ac12b68ccb3f7f4a3d02b54018d35f
Joffreynk <joyjoffrey@gmail.com>
2023-03-14 19:15:32 +0200

    remove white on the query

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,10 @@
 class ArticlesController < ApplicationController
 
   def index
+
     if params[:query].present?
-      @articles = Article.where("LOWER(title) like  ?", "%#{params[:query.downcase]}%")
+      puts "################ #{params[:query.downcase].strip} #{User.find(current_user).id} #########"
+      @articles = Article.where("LOWER(title) like  ?", "%#{params[:query.downcase].strip}%")
     else
       @articles = Article.all
     end
