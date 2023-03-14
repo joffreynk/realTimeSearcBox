b0684806f66d5adc590589f6f5fc44c40252b85b
Joffreynk <joyjoffrey@gmail.com>
2023-03-14 17:33:50 +0200

    create article model

---
+++ db/migrate/20230314153106_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.string :title, null: false
+      t.text :body
+
+      t.timestamps
+    end
+    add_index :articles, :title
+    add_index :articles, :body
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_150919) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_153106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["body"], name: "index_articles_on_body"
+    t.index ["title"], name: "index_articles_on_title"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
