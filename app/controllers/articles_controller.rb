d94efdab99f187fa98541350ab0ffc86419d3616
Joffreynk <joyjoffrey@gmail.com>
2023-03-14 19:36:14 +0200

    testing users queries

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,18 @@
 class ArticlesController < ApplicationController
 
   def index
-
     if params[:query].present?
-      puts "################ #{params[:query.downcase].strip} #{User.find(current_user).id} #########"
+      createSearch(params[:query.downcase].strip)
+      puts "################ #{params[:query.downcase].strip} #{current_user.id} #########"
       @articles = Article.where("LOWER(title) like  ?", "%#{params[:query.downcase].strip}%")
     else
       @articles = Article.all
     end
   end
+
+  def createSearch(query)
+    
+  end
+
+
 end
