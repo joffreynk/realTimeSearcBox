d5bd2e63e55518b653246a79d87ae854099f140f
Joffreynk <joyjoffrey@gmail.com>
2023-03-15 15:47:23 +0200

    sort the by number of appreance

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -2,6 +2,6 @@
 
 class QueriesController < ApplicationController
   def index
-    @queries = Query.where(user_id: current_user.id)
+    @queries = Query.where(user_id: current_user.id).order('times DESC')
   end
 end
